04d99b54d6b6633ef48c114f9bd2154739e46044
Reneginf <reneginf@gmail.com>
2022-06-15 21:58:13 +0000

    test

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-/* eslint-disable */
 import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = ExcuseGen();
+  console.log("Test" + ExcuseGen);
+};
+
+let ExcuseGen = () => {
+  let pronoun = ["A", "The"];
+  let subject = ["mayor", "dragon", "supermodel", "gargoyle", "USSR"];
+  let action = ["ran over my", "ate my", "flew away with my", "seduced my"];
+  let place = ["in Russia", "in space", "in Mexico", "in another dimension"];
+  let item = ["house", "phone", "D&D character", "savings", "best friend"];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let placeIndex = Math.floor(Math.random() * place.length);
+  let itemIndex = Math.floor(Math.random() * item.length);
+
+  return (
+    pronoun[pronounIndex] +
+    " " +
+    subject[subjectIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    item[itemIndex] +
+    " " +
+    place[placeIndex]
+  );
 };
